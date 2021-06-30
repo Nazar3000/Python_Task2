24bff468b5a2956ec35dc42308b217a268aea9e4
Nazar Mazur <nazarii.mazur@gmail.com>
2021-06-30 13:24:25 +0300

    add uotput lists takes 10 min

--- task2.py
+++ task2.py
@@ -3,30 +3,45 @@ import re
 def validator():
     password = input("Input your password")
     list_of_passwords =password.split(',')
-    print(type(list_of_passwords))
-    print(list_of_passwords)
-    x = True
+
+
+    output_list=[]
+    false_output_list = []
     for item in list_of_passwords:
+        x = True
         while x:
 
-            if (len(item)<6 or len(item)>12):
+            if (len(item)<6 or len(item)>20):
+                false_output_list.append(item)
                 break
             elif not re.search("[a-z]", item):
+                false_output_list.append(item)
                 break
             elif not re.search("[0-9]", item):
+                false_output_list.append(item)
                 break
             elif not re.search("[A-Z]", item):
+                false_output_list.append(item)
                 break
-            elif not re.search("[$#@]", item):
+            elif not re.search("[$#@!]", item):
+                false_output_list.append(item)
                 break
             elif re.search("\s", item):
+                false_output_list.append(item)
                 break
             else:
-                print("Valid Password")
-                x=False
-                continue
 
-    if x:
-        print("Not a Valid Password")
+                output_list.append(item)
+
+                x=False
+                break
+    print(output_list)
+    print(false_output_list)
+    return output_list, false_output_list
+
+
+
+
+
 
 validator()
